25aac3fe803ab279755d7055bbfff23d52e0c839
Szalontai József <Jozsef.Szalontai@multisoft.hu>
2022-03-02 15:51:17 +0100

    Added test method to query a set of dictations via POST /app/getdictationinfolist

---
+++ testapps/EnterpriseAppInterface/CSharp/MobileAPITest/Data/GetDictationInfoListRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileAPITest.Data;
+
+internal class GetDictationInfoListRequest
+{
+	public Guid CRI { get; set; }
+	public string[] dictationIds { get; set; }
+}
