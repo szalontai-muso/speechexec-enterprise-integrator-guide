fd76c4dd09aedb017ac9695f8c0ee0f681e60116
Szalontai József <Jozsef.Szalontai@multisoft.hu>
2022-03-04 10:28:09 +0100

    Added attachment download and query user settings test methods

---
+++ testapps/EnterpriseAppInterface/CSharp/MobileAPITest/Data/UserSettingsResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileAPITest.Data;
+
+internal class UserSettingsResponse
+{
+	public bool IsNdevSrEnabled { get; set; }
+	public string? PreferredNdevLanguageCodes { get; set; }
+	public string? SupportedNdevLanguageCodes { get; set; }
+	public bool IsTranscriptionServiceEnabled { get; set; }
+	public bool IsMultiSpeakerTranscriptionEnabled { get; set; }
+}
